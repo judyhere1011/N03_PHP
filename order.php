35ce921bee7313e6e7db7ebc0f24bee904910d80
dovanlong203 <149645282+dovanlong203@users.noreply.github.com>
2023-12-06 15:11:40 +0700

    Add javascript (call API address)

--- order.php
+++ order.php
@@ -1,13 +1,88 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
->
+</body>
+</html>
+
+<?php
+require('./core/database.php');
+require('./core/flash.php');
+require('./core/cart.php');
+$total_amount = 0;
+$cart = new Cart;
+$carts = $cart->all($db);
+
+
+if (isset($_POST['submit'])) {
+    $adress_str = $_POST['adress'] . ' - ' . $_POST['area'];
+    $ship = $_POST['ship_money'];
+    $mess2 = 'Phí Ship:' . ' ' . strval(number_format($ship)) . 'VNĐ';
+    $user_id = 0;
+    $data = [
+        'user_id' => $user_id,
+        'user_name' => $_POST['name'],
+        'user_email' => $_POST['email'],
+        'user_address' => $adress_str,
+        'user_phone' => $_POST['phone'],
+        'message' => $mess2,
+        'amount' => $total_amount + $ship,
+        'payment' => '',
+    ];
+    $kq = $db->create('transaction', $data);
+    $transaction_id = $db->getLastId('transaction');
+    foreach ($carts as $items) {
+        $data = [
+            'transaction_id' => $transaction_id['id'],
+            'product_id' => $items['id'],
+            'qty' => $items['quantity'],
+            'amount' => $items['sub_total'],
+            'size_id' => $items['size_id']
+        ];
+        $db->create('order', $data);
+
+        //Cộng lượt mua
+        $product = $db->getById('product', $items['id']);
+        $sl = $product['buyed'] + intval($items['quantity']);
+        $data4 = [
+            'buyed' => $sl
+        ];
+        $db->update('product', $data4, $product['id']);
+
+        //trừ số lượng
+        $size_detail = $db->getFirst("select * from sizedetail where product_id = {$items['id']} and size_id = {$items['size_id']}");
+        if ($size_detail) {
+            $id_update_size = $size_detail['id'];
+            $amount = $size_detail['quantity'] - $items['quantity'];
+            if ($id_update_size != 0 && $amount > 0) {
+                $data2 = [
+                    'product_id' => $items['id'],
+                    'size_id' => $items['size_id'],
+                    'quantity' => $amount,
+                ];
+                $db->update('sizedetail', $data2, $id_update_size);
+            } else if ($id_update_size != 0 && $amount == 0) {
+                $db->delete('sizedetail', $id_update_size);
+            }
+        }
+    }
+    $cart->empty();
+    Flash::set('message', "Đặt hàng thành công, chúng tôi sẽ liên hệ với bạn để giao hàng");
+    echo '<script>';
+    echo 'alert("Đặt hàng thành công, chúng tôi sẽ liên hệ với bạn để giao hàng");';
+    echo 'window.location.href = "./index.php";';
+    echo '</script>';
+}
+
+if (count($carts) == 0) {
+    header('location: ./index.php');
+}
+
+foreach ($carts as $item) {
+    $total_amount += $item['sub_total'];
+}
+
+$shipping = $db->getAll("select * from shipping order by id asc");
+
+?>
+<?php include('./layout/head.php') ?>
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 clearpadding" style="margin-top: 15px;">
         <?php include('./layout/sidebar.php') ?>
@@ -69,6 +144,14 @@
                                                 <td><input style="max-width: 200px" id="adress" name="adress" type="hidden" value=""></td>
                                             </tr>
                                             <tr>
+                                                <td>Dịch vụ vận chuyển</td>
+                                                <td>
+                                                    <select class="form-control" name="ship" id="ship" style="max-width: 200px;padding: 5px">
+                                                        <option value>--Chọn Dịch Vụ--</option>
+                                                    </select>
+                                                </td>
+                                            </tr>
+                                            <tr>
                                                 <td>Tổng tiền</td>
                                                 <td>
                                                     <span class="money"><?php echo number_format($total_amount); ?></span>
@@ -78,7 +161,7 @@
                                             <tr>
                                                 <td>Phí ship</td>
                                                 <td>
-                                                    <span id="ship_label"> 0</span>
+                                                    <span id="ship_label">0</span>
                                                 </td>
                                                 <td>VNĐ<input style="max-width: 100px" type="hidden" name="ship_money" id="ship_money" value="0"></td>
                                             </tr>
@@ -86,6 +169,7 @@
                                                 <td>Thành tiền</td>
                                                 <td style="color: red;max-width: 50px">
                                                     <span id="total_amount"><?php echo number_format($total_amount); ?></span>
+                                                    <input type="hidden" name="tempt_amount" value="<?php echo $total_amount; ?>" />
                                                 </td>
                                                 <td>VNĐ</td>
                                             </tr>
@@ -97,6 +181,13 @@
                                             </tr>
                                         </tbody>
                                     </table>
+                                    <div class="col-lg-8">
+                                        <div class="col-lg-5">
+                                            <image style="width: 100%; height: 100%" src="./public/images/van_chuyen.png" />
+                                        </div>
+                                        <div class="col-lg-5">Đơn vị vận chuyển<p>GHN - Giao hàng nhanh toàn quốc</p>
+                                        </div>
+                                    </div>
                                     <button style="min-width: 100px;float: right;margin-top: 50px" type="submit" name="submit" class="btn btn-success">Xác nhận</button>
                                 </form>
                             </div>
@@ -107,3 +198,262 @@
         </div>
     </div>
 </div>
+<?php include('./layout/footer.php') ?>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $.ajax({
+            url: 'https://online-gateway.ghn.vn/shiip/public-api/master-data/province',
+            type: 'POST',
+            dataType: 'json',
+            headers: {
+                'token': '464ef410-6fc8-11ec-9054-0a1729325323'
+            },
+            contentType: 'application/json; charset=utf-8',
+            success: function(result) {
+                // CallBack(result);
+                $.each(result, function(key, value) {
+                    if (key.includes("data")) {
+                        $.each(value, function(key2, value2) {
+                            $('<option>').val(value2.ProvinceID).text(value2.ProvinceName).appendTo('#province');
+                        })
+                    }
+                })
+            },
+            error: function(error) {}
+        });
+    });
+</script>
+<script type="text/javascript">
+    function formatNumber(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    }
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#province').change(function(envent) {
+            var province = $('#province').val();
+            province = parseInt(province);
+
+            $('#district')
+                .empty()
+                .append('<option value>--Chọn Quận,Huyện--</option>');
+            $('#ward')
+                .empty()
+                .append('<option value>--Chọn Phường,Xã--</option>');
+            $('#ship')
+                .empty()
+                .append('<option value>--Chọn Dịch Vụ--</option>');
+
+            $('#ship_money').val('0');
+            $('#ship_label').html('0');
+            var total_count = <?php echo $total_amount ?>;
+            $('#total_amount').html(formatNumber(total_count));
+
+
+            $.ajax({
+                url: 'https://online-gateway.ghn.vn/shiip/public-api/master-data/district',
+                type: 'GET',
+                dataType: 'json',
+                headers: {
+                    'token': '464ef410-6fc8-11ec-9054-0a1729325323',
+                },
+                data: {
+                    province_id: province
+                },
+                contentType: 'application/json; charset=utf-8',
+                success: function(result) {
+                    // CallBack(result);
+                    $.each(result, function(key, value) {
+                        if (key.includes("data")) {
+                            $.each(value, function(key2, value2) {
+                                $('<option>').val(value2.DistrictID).text(value2.DistrictName).appendTo('#district');
+                            })
+                        }
+
+                    })
+
+                },
+                error: function(error) {
+
+                }
+            });
+        });
+    });
+</script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#district').change(function(envent) {
+            var district = $('#district').val();
+            district = parseInt(district);
+            $('#ward')
+                .empty()
+                .append('<option value>--Chọn Phường,Xã--</option>');
+            $('#ship')
+                .empty()
+                .append('<option value>--Chọn Dịch Vụ Vận Chuyển--</option>');
+            $('#ship_money').val('0');
+            $('#ship_label').html('0');
+            var total_count = <?php echo $total_amount ?>;
+            $('#total_amount').html(formatNumber(total_count));
+            $.ajax({
+                url: 'https://online-gateway.ghn.vn/shiip/public-api/master-data/ward',
+                type: 'GET',
+                dataType: 'json',
+                headers: {
+                    'token': '464ef410-6fc8-11ec-9054-0a1729325323',
+                },
+                data: {
+                    district_id: district
+                },
+                contentType: 'application/json; charset=utf-8',
+                success: function(result) {
+                    // CallBack(result);
+                    $.each(result, function(key, value) {
+                        if (key.includes("data")) {
+                            $.each(value, function(key2, value2) {
+                                $('<option>').val(value2.WardCode).text(value2.WardName).appendTo('#ward');
+                            })
+                        }
+                    })
+                },
+                error: function(error) {}
+            });
+            var dis = <?php echo $shipping[0]['id_district'] ?>;
+            $.ajax({
+                url: 'https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/available-services',
+                type: 'GET',
+                dataType: 'json',
+                headers: {
+                    'token': '464ef410-6fc8-11ec-9054-0a1729325323',
+                },
+                data: {
+                    "shop_id": 2413002,
+                    "from_district": dis,
+                    "to_district": district
+                },
+                contentType: 'application/json; charset=utf-8',
+                success: function(result) {
+                    // CallBack(result);
+                    $.each(result, function(key, value) {
+                        if (key.includes("data")) {
+                            $.each(value, function(key2, value2) {
+                                $('<option>').val(value2.service_id).text("GHN_Đường bộ").appendTo('#ship');
+                            })
+                        }
+                    })
+                },
+                error: function(error) {
+                    alert("Không thể giao hàng đến Quận,Huyện này!");
+                }
+            });
+        });
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#ward').change(function(envent) {
+            $('#ship')
+                .empty()
+                .append('<option value>--Chọn Dịch Vụ Vận Chuyển--</option>');
+            $('#ship_money').val('0');
+            $('#ship_label').html('0');
+            var total_count = <?php echo $total_amount ?>;
+            $('#total_amount').html(formatNumber(total_count));
+            var district = $('#district').val();
+            district = parseInt(district);
+            var a = $("#province option:selected").text();
+            var b = $("#district option:selected").text();
+            var c = $("#ward option:selected").text();
+            var str = a + ", " + b + ", " + c;
+            $('#adress').val(str);
+            var dis = <?php echo $shipping[0]['id_district'] ?>;
+            $.ajax({
+                url: 'https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/available-services',
+                type: 'GET',
+                dataType: 'json',
+                headers: {
+                    'token': '464ef410-6fc8-11ec-9054-0a1729325323',
+                },
+                data: {
+                    "shop_id": 2413002,
+                    "from_district": dis,
+                    "to_district": district
+                },
+                contentType: 'application/json; charset=utf-8',
+                success: function(result) {
+                    // CallBack(result);
+                    $.each(result, function(key, value) {
+                        if (key.includes("data")) {
+                            $.each(value, function(key2, value2) {
+                                if(key2 == 0) {
+                                    $('<option>').val(value2.service_id).text("GHN_Đường bộ").appendTo('#ship');
+                                }
+                            })
+                        }
+                    })
+                },
+                error: function(error) {
+                    alert("Không thể giao hàng đến Quận,Huyện này!");
+                }
+            });
+        });
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#ship').change(function(envent) {
+            $('#ship_money').val('0');
+            $('#ship_label').html('0');
+            var total_count = <?php echo $total_amount ?>;
+            $('#total_amount').html(formatNumber(total_count));
+            var ship = $('#ship').val();
+            ship = parseInt(ship);
+            var district = $('#district').val();
+            district = parseInt(district);
+            var ward = $('#ward').val();
+            ward = parseInt(ward);
+            var money = $('.money');
+            var money_total = money.text();
+            money_total = parseFloat(money_total) * 100;
+            var s1 = $('#ship_label');
+            $.ajax({
+                url: 'https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee',
+                type: 'GET',
+                dataType: 'json',
+                headers: {
+                    'token': '464ef410-6fc8-11ec-9054-0a1729325323',
+                },
+                data: {
+                    "service_id": ship,
+                    "insurance_value": money_total,
+                    "coupon": null,
+                    "from_district_id": <?php echo $shipping[0]['id_district'] ?>,
+                    "to_district_id": district,
+                    "to_ward_code": ward,
+                    "height": 15,
+                    "length": 15,
+                    "weight": 1000,
+                    "width": 15
+                },
+                contentType: 'application/json; charset=utf-8',
+                success: function(result) {
+                    // CallBack(result);
+                    $.each(result, function(key, value) {
+                        if (key.includes("data")) {
+                            $.each(value, function(key2, value2) {
+                                if (key2.includes("total")) {
+                                    $('#ship_money').val(parseFloat(value2));
+                                    $('#ship_label').html(formatNumber(value2));
+                                    var total_count = <?php echo $total_amount ?> + value2;
+                                    $('#total_amount').html(formatNumber(total_count));
+                                }
+                            })
+                        }
+                    })
+                },
+                error: function(error) {}
+            });
+        });
+    });
+</script>
