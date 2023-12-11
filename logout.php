dcacd7b2db91f89282891c3e20db1648a9aa27a6
dovanlong203 <149645282+dovanlong203@users.noreply.github.com>
2023-12-11 23:09:08 +0700

    Chuyển file logout.php theo OOP

--- logout.php
+++ logout.php
@@ -1,5 +1,27 @@
-<?php 
-	session_start();
-	session_destroy();
-	header('location: index.php');
- ?>
+<?php
+
+class AdminLogout
+{
+    public function __construct()
+    {
+        session_start();
+        $this->logout();
+    }
+
+    private function logout()
+    {
+        if (isset($_SESSION['admin'])) {
+            unset($_SESSION['admin']);
+        }
+
+        if (isset($_COOKIE['admin'])) {
+            setcookie('admin', '', time() - 1, '/');
+        }
+
+        header('location: ./login.php');
+    }
+}
+
+// Sử dụng đối tượng AdminLogout
+$adminLogout = new AdminLogout();
+?>
