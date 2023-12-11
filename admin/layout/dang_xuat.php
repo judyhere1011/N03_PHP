c56032cc628a6ff593c7af61be9c9c7e73d60ec2
hnga259 <149616643+hnga259@users.noreply.github.com>
2023-12-12 00:39:33 +0700

    Delete admin/layout directory

--- admin/layout/dang_xuat.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['admin'])) {
-    unset($_SESSION['admin']);
-}
-if (isset($_COOKIE['admin'])) {
-    setcookie('admin', '', time()-1, '/');
-}
-header('location: ./dang_nhap.php');
